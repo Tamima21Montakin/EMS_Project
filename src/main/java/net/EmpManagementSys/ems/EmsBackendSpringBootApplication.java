feda494c1d0e5f2e60682a13e1239abf864e3ff0
atb_dEv1 <atb_dEv1@porters.jp>
2025-02-07 18:07:24 +0600

    EMS project

---
+++ src/main/java/net/EmpManagementSys/ems/EmsBackendSpringBootApplication.java
@@ -0,0 +1,13 @@
+package net.EmpManagementSys.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsBackendSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsBackendSpringBootApplication.class, args);
+	}
+
+}
